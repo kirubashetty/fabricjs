24b0665ad4dfa26e7d1746990ba160d41da8a17d
kirubashetty <58547583+kirubashetty@users.noreply.github.com>
2022-05-04 13:14:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+var canvas = new fabric.Canvas('canvas');
+document.getElementById("uploader").onchange = function(e) {
+  var reader = new FileReader();
+  reader.onload = function(e) {
+    var image = new Image();
+    image.src = e.target.result;
+    image.onload = function() {
+      var img = new fabric.Image(image);
+      img.set({
+        left: canvas.width/4
+       
+      });
+      img.scaleToWidth(canvas.width);
+      img.scaleToHeight(canvas.height);
+      canvas.add(img).setActiveObject(img).renderAll();
+    }
+  }
+  reader.readAsDataURL(e.target.files[0]);
+ 
+}
+canvas.on('mouse:wheel', function(opt) {
+  var delta = opt.e.deltaY;
+  var zoom = canvas.getZoom();
+  zoom *= 0.999 ** delta;
+  if (zoom > 20) zoom = 20;
+  if (zoom < 1) zoom = 1;
+  canvas.setZoom(zoom);
+  opt.e.preventDefault();
+  opt.e.stopPropagation();
+})
+canvas.on('mouse:down', function(options) {
+  if(options.target==null)
+  {
+    canvas.setZoom(1);
+  }
+});
